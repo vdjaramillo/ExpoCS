fb7ef2a10ca7f31121cc7b6a2e0ad971b3bb5c67
vdjaramillo <33434357+vdjaramillo@users.noreply.github.com>
2019-08-13 19:45:30 -0500

    Update Ejemplo1.java

--- Ejemplo1.java
+++ Ejemplo1.java
@@ -1,1 +1,6 @@
-
+import java.util.LinkedList;
+public interface Servidor{
+	LinkedList<Cliente> usuarios = new LinkedList<>();
+	LinkedList<Hotel> hoteles = new LinkedList<>();
+	LinkedList<Reserva> reservas = new LinkedList<>();
+}
