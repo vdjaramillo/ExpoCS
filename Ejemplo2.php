49b8e9ebad4cccff57e8ca0d476abc9090021ac9
vdjaramillo <33434357+vdjaramillo@users.noreply.github.com>
2019-08-13 19:46:03 -0500

    Update Ejemplo2.php

--- Ejemplo2.php
+++ Ejemplo2.php
@@ -1,1 +1,17 @@
 
+<?php
+$enlace = new mysqli('localhost', 'root', '123456789', 'table');
+if (!$enlace) {
+    echo 'No pudo conectarse a mysql'.$enlace->connect_error;
+    exit;
+}else{
+    echo 'Conectado';
+}
+$quer = 'INSERT INTO usuario (nombre, contrasena) VALUES ("'.$_POST['j_username'].'","'.$_POST['j_password'].'");';
+
+if (!($enlace->query($quer)===TRUE)) {
+	echo "Error de BD, no se pudo consultar la base de datos\n".$enlace->connect_error;
+}
+
+$enlace->close();
+?>
